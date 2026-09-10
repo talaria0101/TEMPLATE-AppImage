b4c09560a67e9347f4fd48712860d95600f2a260
Talaria <324092415+talaria0101@users.noreply.github.com>
2026-09-10 21:59:16 +0000

    consume anylinux-setup-action v3, drop the appimagetool workaround
    The emulated job now fetches setup.sh from the released
    anylinux-setup-action (commit pinned) instead of carrying an inline
    copy of the setup. appimagetool 0.4.2 detects ppc64le correctly, the
    APPIMAGE_ARCH export is no longer needed and is removed.

--- make-appimage.sh
+++ make-appimage.sh
@@ -4,9 +4,7 @@ set -eu
 
 ARCH=$(uname -m)
 VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
-# appimagetool misdetects ppc64le as ppc64 (rust reports powerpc64 for both),
-# pin the runtime arch to the uname value
-export ARCH VERSION APPIMAGE_ARCH="$ARCH"
+export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
