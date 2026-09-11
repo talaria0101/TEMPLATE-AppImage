c4f4b2776e257041060185aafb0a97a7fa9aa950
Talaria <324092415+talaria0101@users.noreply.github.com>
2026-09-11 13:16:10 +0545

    skip debloated packages on arches they do not ship for

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,9 +8,15 @@ echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 # pacman -Syu --noconfirm PACKAGESHERE
 
-echo "Installing debloated packages..."
-echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+# get-debloated-pkgs only ships x86_64 and aarch64 builds, the other arches
+# stay on the stock pacman packages until it grows them
+case "$ARCH" in
+	x86_64|aarch64)
+		echo "Installing debloated packages..."
+		echo "---------------------------------------------------------------"
+		get-debloated-pkgs --add-common --prefer-nano
+		;;
+esac
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
